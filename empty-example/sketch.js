89871e46a8df83cad60f5fc7bf182ead6ce513b9
michaellabuno <83736390+michaellabuno@users.noreply.github.com>
2021-08-30 01:43:44 -0700

    script addition

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,3 +1,16 @@
+<html>
+
+<head>
+
+<script src="https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.js">
+</script>
+
+<script src="sketch.js"></script>
+
+</head>
+
+<body>
+  
 function setup() {
   createCanvas(400,400);
 }
@@ -10,3 +23,6 @@ if (mouseIsPressed){
 }
 ellipse(mouseX,mouseY,80,80);
 }
+</body>
+
+</html>
