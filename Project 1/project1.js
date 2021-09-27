32fb026413ff22a166d094ef006d33fa63e94060
michaellabuno <83736390+michaellabuno@users.noreply.github.com>
2021-09-26 23:07:05 -0700

    Add files via upload

--- Project 1/project1.js
+++ Project 1/project1.js
@@ -1,14 +1,18 @@
 let girlImage1;
 
 function preload() {
-    girlImage1 = loadImage('https://github.com/michaellabuno/michaellabuno.github.io/blob/main/lofigirl.gif');
+    girlImage1 = loadImage('https://michaellabuno.github.io/Project%201/lofigirl.gif');
 }
 
 
 function setup() {
     createCanvas(400, 400);
+    brackground('gray');
+    image(girlImage1, 0, 0);
 }
 
 function draw() {
+    circle(200, 200, 50);
+
 
 }
