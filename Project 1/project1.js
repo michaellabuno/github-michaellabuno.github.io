b76fa0d58766eeaa19959b55e219a5a9633b1e06
michaellabuno <83736390+michaellabuno@users.noreply.github.com>
2021-09-26 22:59:06 -0700

    Add files via upload
    add files

---
+++ Project 1/project1.js
@@ -0,0 +1,14 @@
+let girlImage1;
+
+function preload() {
+    girlImage1 = loadImage('https://github.com/michaellabuno/michaellabuno.github.io/blob/main/lofigirl.gif');
+}
+
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+
+}
